513b2d207dbea00b66ec0a7d50424bfeeb873f59
kamil <kamil.partyka1998@gmail.com>
2022-07-20 12:30:31 +0200

    After ex.13

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity.config;
 
 import org.springframework.boot.autoconfigure.graphql.ConditionalOnGraphQlSchema;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -8,6 +9,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -17,14 +25,43 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/api/user" )
-                .hasAnyRole("user","admin")
+                .hasAnyRole("USER","ADMIN")
                 .antMatchers("/api/admin" )
-                .hasRole("admin")
+                .hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails user=
+                User.builder()
+                        .username("user")
+                        .password(encoder().encode("user"))
+                        .roles("USER")
+                        .build();
+UserDetails admin=
+        User.builder()
+                .username("admin")
+                .password(encoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(
+                user,
+                admin
+        );
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
